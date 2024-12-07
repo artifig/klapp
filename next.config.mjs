b2c389c485ce7907dcaa7a32bd8f5335421148f7
Otto Mättas <ottomattas@gmail.com>
2024-12-07 18:27:59 +0200

    Try without output

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,8 @@ if (isGithubActions) {
   // Get the repository name from the environment variable
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
   nextConfig = {
-    assetPrefix: `/${repo}/`,
+    assetPrefix: `/${repo}`,
     basePath: `/${repo}`,
-    output: 'export',
   };
 }
 
