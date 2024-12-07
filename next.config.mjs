991f98e46552be46239ad292badbcdf45108a717
Otto Mättas <ottomattas@gmail.com>
2024-12-07 18:27:00 +0200

    Fix path for assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ if (isGithubActions) {
   // Get the repository name from the environment variable
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
   nextConfig = {
-    assetPrefix: `/${repo}/public/`,
+    assetPrefix: `/${repo}/`,
     basePath: `/${repo}`,
     output: 'export',
   };
