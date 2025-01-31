fb13ca18a15d4fd249932e660a7a27073a97caf6
Otto Mättas <ottomattas@gmail.com>
2025-01-31 02:37:46 +0200

    Fix potential undefined completedCategories in AssessmentContextCard

--- src/components/context/AssessmentContextCard.tsx
+++ src/components/context/AssessmentContextCard.tsx
@@ -35,7 +35,7 @@ export const AssessmentContextCard = () => {
   const { 
     categories,
     currentCategory,
-    completedCategories,
+    completedCategories = [],
     setCurrentCategory,
     progress 
   } = useAssessmentContext();
