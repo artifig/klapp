2cdb047110f953a357c9b418bb790402631a276e
Otto Mättas <ottomattas@gmail.com>
2025-02-12 20:20:09 +0200

    Simplify UiButton component with streamlined type definition

--- src/components/ui/UiButton.tsx
+++ src/components/ui/UiButton.tsx
@@ -1,17 +1,16 @@
 import * as React from "react"
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
-
-const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ ...props }, ref) => {
-    return (
-      <button
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
+const Button = React.forwardRef<
+  HTMLButtonElement,
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+>((props, ref) => {
+  return (
+    <button
+      ref={ref}
+      {...props}
+    />
+  )
+})
 Button.displayName = "Button"
 
 export { Button } 
